c508e9774f54648d29a92e16e5a71b8674de51cf
antraahex <antra@ahex.co.in>
2021-03-10 11:07:17 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
+import { DatePipe } from '@angular/common';
 
+export interface event {
+  title: string,
+  date: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'calendar';
+  showForm: boolean = false;
+  showEditForm: boolean = false;
+  date: string;
+  task: event;
+  events: event[] = [];
+  editedTask: event;
+  editedIndex: number;
+  editedTitle: string;
+  form = new FormGroup({
+    title: new FormControl("", Validators.required)
+  });
+
+  editForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required)
+
+  });
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    dateClick: this.handleDateClick.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    events: []
+  };
+
+  constructor(public datepipe: DatePipe) {
+
+  }
+  ngOnInit(): void {
+    this.update();
+  }
+
+  public update() {
+    let initialValue = localStorage.getItem('events');
+    if (initialValue == null) {
+      this.events = [];
+      this.calendarOptions.events = [];
+    }
+    else {
+      this.events = JSON.parse(localStorage.getItem('events'));
+      this.calendarOptions.events = JSON.parse(localStorage.getItem('events'));
+    }
+
+  }
+
+  public handleDateClick(arg) {
+    this.showForm = true;
+    this.date = arg.dateStr;
+  }
+
+  public handleEventClick(e) {
+    this.openEditForm(e);
+    this.showEditForm = true;
+  }
+
+  public addTask(val, date): void {
+    this.task = {
+      title: val.title, date: date
+    }
+    this.events.push(this.task);
+    localStorage.setItem('events', JSON.stringify(this.events));
+    this.calendarOptions.events = JSON.parse(localStorage.getItem('events'));
+    this.form.reset();
+    this.showForm = false;
+  }
+
+  public editTask(val): void {
+    this.events[this.editedIndex] = val;
+    localStorage.setItem('events', JSON.stringify(this.events));
+    this.calendarOptions.events = JSON.parse(localStorage.getItem('events'));
+    this.showEditForm = false;
+
+  }
+
+  public deleteTask(val): void {
+    for (let i of this.events) {
+      if (i.title == val.title) {
+        this.events.splice(this.events.indexOf(i), 1);
+        localStorage.setItem('events', JSON.stringify(this.events));
+        this.calendarOptions.events = JSON.parse(localStorage.getItem('events'));
+        this.showEditForm = false;
+      }
+    }
+  }
+
+  public openEditForm(val): void {
+    this.editForm = new FormGroup({
+      title: new FormControl(val.event.title, Validators.required),
+      date: new FormControl(this.datepipe.transform(val.event.start, 'yyyy-MM-dd'), Validators.required)
+    });
+    this.showEditForm = true;
+    this.date = this.datepipe.transform(val.event.start, 'yyyy-MM-dd');
+    this.editedTask = this.editForm.value;
+    this.editedIndex = this.events.findIndex((elem) => { return elem.title === this.editedTask.title });
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from '@angular/common';
 
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ButtonModule,
+    DialogModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
